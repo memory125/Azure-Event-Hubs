33136bb4c8bebd4a905ba0134f1c731007f029db
memory125 <mengde1120@sina.com>
2016-11-17 14:29:52 +0800

    Create Service Bus Sample

---
+++ ServiceBusTest/Receiver/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Service Bus and Queue are created on Azure portal
+            var connectionString = "{You Service Bus Connection String}";
+            var queueName = "{Your Queue Name}";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ServiceBusTest/ServiceBusTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+
+namespace ServiceBusTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Service Bus and Queue are created on Azure portal
+            var connectionString = "{You Service Bus Connection String}";
+            var queueName = "{Your Queue Name}";
+            var number = 1;
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+                      
+            while (true)
+            {
+                var infoMessage = string.Format("ServiceBus-Demo-De! This is the {0} message!", number);
+                var message = new BrokeredMessage(infoMessage);
+                client.Send(message);
+                Console.WriteLine(infoMessage);
+                number++;
+            }
+        }
+    }
+}
