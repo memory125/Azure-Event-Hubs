8d8b958f856b9696d000cde1a857650efe5046d4
memory125 <mengde1120@sina.com>
2016-11-17 10:41:29 +0800

    Create the event hubs demo projects

---
+++ EventHubsTest/Reveiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Reveiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "{Your Event Hubs Connection String}";
+            string eventHubName = "{Event Hub Name}";
+            string storageAccountName = "{Account Storage Name}";
+            string storageAccountKey = "{Account Storage Key}";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            var options = new EventProcessorOptions();
+            options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+
+    }
+}
